484658043829ddab82bbf959f678ea2e4da26598
Jean Pires <jean.m.pires@gmail.com>
2024-02-12 12:18:24 -0300

    str-producer started

---
+++ str-producer/src/test/java/com/jeanpires/strproducer/StrProducerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jeanpires.strproducer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StrProducerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
