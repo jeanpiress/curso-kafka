592560f2b0fbae0991cb5551a3d2e85ba15e364d
Jean Pires <jean.m.pires@gmail.com>
2024-02-13 07:58:36 -0300

    Criando custumListener

--- str-consumer/src/main/java/com/jeanpires/strconsumer/listeners/StrConsumerListener.java
+++ str-consumer/src/main/java/com/jeanpires/strconsumer/listeners/StrConsumerListener.java
@@ -1,5 +1,6 @@
 package com.jeanpires.strconsumer.listeners;
 
+import com.jeanpires.strconsumer.custom.StrConsumerCustonListener;
 import lombok.extern.log4j.Log4j2;
 import org.springframework.kafka.annotation.KafkaListener;
 import org.springframework.stereotype.Component;
@@ -8,17 +9,17 @@ import org.springframework.stereotype.Component;
 @Log4j2
 public class StrConsumerListener {
 
-    @KafkaListener(groupId = "group-1", topics = "str-topic", containerFactory = "strContainerFactory")
+    @StrConsumerCustonListener(groupId = "group-1")
     public void create(String message){
         log.info("CREATE ::: Recive message {}", message);
     }
 
-    @KafkaListener(groupId = "group-1", topics = "str-topic", containerFactory = "strContainerFactory")
+    @StrConsumerCustonListener(groupId = "group-1")
     public void log(String message){
         log.info("LOG ::: Recive message {}", message);
     }
 
-    @KafkaListener(groupId = "group-1", topics = "str-topic", containerFactory = "strContainerFactory")
+    @StrConsumerCustonListener(groupId = "group-2")
     public void history(String message){
         log.info("HISTORY ::: Recive message {}", message);
     }
